6b6ba1f050c05562662a019b8d1c57b33aee9d1f
Adrian Maolepszy <adr_mal@wp.pl>
2016-12-07 14:59:24 +0100

    proofReader 2

--- ProofReader/src/corrector/CorrectorMachine.java
+++ ProofReader/src/corrector/CorrectorMachine.java
@@ -1,6 +1,11 @@
 package corrector;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CorrectorMachine {
+
+	private List<String> listOfAllWords;
 
 	public static String getCorrectedText(String text) {
 		for(String regex : CorrectorDatabase.regexes.keySet()) {
@@ -11,4 +16,41 @@ public class CorrectorMachine {
 		return text;
 	}
 	
+	public static String getCorrectedText2(String text) {
+		Pattern pattern = Pattern.compile("\\b\\p{L}+\\b");
+
+		List<String> wordsInText = getWordsInLine(text);
+		for(String word : wordsInText) {
+			if(!listOfAllWords.contains(word)) {
+				// wyszukanie słowa różniącego się jedną literą
+				text.replaceAll("\\b" + word + "\\b", similarWord);
+			}
+		}
+
+		return text;
+	}
+
+	public static void initializeListOfAllWords() {
+		// otwarcie pliku
+
+		listOfAllWords = new ArrayList<>();
+
+		while(file.hasNextLine()) {
+			List<String> words = getWordsInLine(file.nextLine());
+			listOfAllWords.addAll(words);
+		}
+	}
+
+	private static List<String> getWordsInLine(String line) {
+		Pattern pattern = Pattern.compile("\\b\\p{L}+\\b");
+
+		List<String> words = new ArrayList<String>();
+ 		Matcher m = pattern.matcher(text);
+ 		while(m.find()) {
+   			words.add(m.group());
+ 		}
+
+		return words;
+	}
+
 }
